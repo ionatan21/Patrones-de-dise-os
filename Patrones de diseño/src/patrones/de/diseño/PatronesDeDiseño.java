c11a859c557bdb40c44a4a1037cd508ce485e6fc
ionatan21 <99590392+ionatan21@users.noreply.github.com>
2022-04-23 09:49:16 -0600

    Add files via upload

---
+++ Patrones de diseño/src/patrones/de/diseño/PatronesDeDiseño.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patrones.de.diseño;
+
+/**
+ *
+ * @author barri
+ */
+public class PatronesDeDiseño {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
